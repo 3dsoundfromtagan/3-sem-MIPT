cd26632ca18da7f054e5744e979f971af2360b19
3dsoundfromtagan <ostapiv.alex@gmail.com>
2017-09-14 17:51:14 +0300

    new

--- ex5.c
+++ ex5.c
@@ -10,32 +10,19 @@ int main (int argc, char* argv[])
 	}
 	
 	int i = atoi(argv[1]);
+	int pid = fork();
 	while (i > 0)
 	{
-		int a = fork();
-		if (!a)
+		if (!pid)
 		{
-			printf("I'm a process. Parent's PID is %d, my PID is %d\n", getppid(), getpid());
-			i--;
+			pid = fork();
+			if (!pid)
+			{
+				printf("I'm a process. Parent's PID is %d, my PID is %d\n", getppid(), getpid());
+			}
 		}
 		i--;
 	}
-	
 
-	
-	/*if (a > 0)
-	{
-		printf("I'm a parent. My PID is %d, child PID is %d\n", getpid(), a);
-	}
-	if (!a)
-	{
-		printf("I'm a process. Parent's PID is %d, my PID is %d\n", getppid(), getpid());
-	}
-	
-	if (a < 0)
-	{
-		printf("Error\n");
-	}*/
-	
 	return 0;
 }
