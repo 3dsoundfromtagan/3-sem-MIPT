0d0a483ef146f8c18a051ddc1c020c9f8b233029
3dsoundfromtagan <ostapiv.alex@gmail.com>
2017-09-18 02:14:25 +0300

    polska crew!

--- ex5.c
+++ ex5.c
@@ -5,12 +5,23 @@ int main (int argc, char* argv[])
 {
 	if (argc != 2)
 	{
-		printf("Only 1 argument!\n");
-		return -1;
+		printf("Only 1 argument\n");
+		exit(-1);
 	}
 	
 	int i = atoi(argv[1]);
+	
+	if (i <= 0)
+	{
+		printf("Number must be natural\n");
+		exit(-1);
+	}
+	
 	int pid = fork();
+	if (pid == -1)
+	{
+		exit(-1);
+	}
 	while (i > 0)
 	{
 		if (!pid)
