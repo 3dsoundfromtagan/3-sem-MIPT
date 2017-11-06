5db20203b87c8fb48bb147d4100cd503554ebe64
merfemor <efremov.roman98@gmail.com>
2017-11-06 03:57:28 +0300

    protocol.h

--- client-server/client.c
+++ client-server/client.c
@@ -7,89 +7,59 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/ipc.h>
 #include <sys/msg.h>
-#include <errno.h>
 #include <unistd.h>
+#include "protocol.h"
 
-#define LAST_MESSAGE 200
-#define MSGSZ 128
 #define CMD_LEN 20
 
-#define WRALL_TYPE 254
-#define SERVER_TYPE 255
-#define QUIT_TYPE 256
+int msqid;
 
-typedef struct _msg {
-    long type;
-    struct {
-        pid_t pid;
-        char text[MSGSZ];
-    } info;
-} msg;
-
-int main() {
-    char command[CMD_LEN];
-    int msqid;
-    key_t key;
-    msg my_msg;
-    pid_t pid = getpid();
-    //size_t msg_length;
-    //int i = 0;
-
-    if ((key = ftok("./server", 0)) < 0) {
-        perror("ftok");
-        exit(-1);
-    }
-
-    if ((msqid = msgget(key, 0666 | IPC_CREAT | IPC_EXCL)) < 0) {
-        if (errno != EEXIST) {
-            perror("Can't creat msgq");
-            exit(-2);
-        } else {
-            if ((msqid = msgget(key, 0)) < 0) {
-                perror("Can't find msgq");
-                exit(-3);
-            }
-        }
-    }
-
-    my_msg.type = 255;
-    strcpy(my_msg.info.text, "Connected");
-    my_msg.info.pid = pid;
-
-    //msg_length = strlen((my_msg)->info.text) + 1;
-    if (msgsnd(msqid, &my_msg, MSGSZ, 0) < 0) {
+void send_message(msg *const message) {
+    message->type = SERVER_TYPE;
+    message->info.pid_from = getpid();
+    if (msgsnd(msqid, message, MSGSZ, 0) < 0) {
         perror("msgsnd");
         exit(-4);
     }
+}
+
+
+int main() {
+    char command[CMD_LEN];
+    key_t key;
+    msg my_msg;
+    pid_t pid = getpid();
+    key = get_key_or_exit();
+    msqid = get_msqid_or_exit(key);
+    printf("Started client with pid = %d, msqid = %d, key = %d\n", pid, msqid, key);
+
+    my_msg.info.comm_type = CONNECT;
+    send_message(&my_msg);
 
     while (1) {
         scanf("%s", command);
         if (strcmp(command, "write") == 0) {
+            scanf("%d", &(my_msg.info.pid_to));
+            scanf("%s", my_msg.info.text);
+            my_msg.info.comm_type = WRITE;
+            send_message(&my_msg);
 
-        }
+        } else if (strcmp(command, "wrall") == 0) {
+            scanf("%s", my_msg.info.text);
+            my_msg.info.comm_type = WRITE_ALL;
+            send_message(&my_msg);
 
-        if (strcmp(command, "wrall") == 0) {
-
-        }
-
-        if (strcmp(command, "read") == 0) {
+        } else if (strcmp(command, "read") == 0) {
             if (msgrcv(msqid, &my_msg, MSGSZ, pid, 0) < 0) {
                 perror("msgrcv");
                 exit(-6);
             }
             printf("%s\n", my_msg.info.text);
-        }
-        if (strcmp(command, "quit") == 0) {
-            my_msg.type = SERVER_TYPE;
-            strcpy(my_msg.info.text, "Disonnected");
-            my_msg.info.pid = pid;
-            //msg_length = strlen((my_msg)->text) + 1;
-            if (msgsnd(msqid, &my_msg, MSGSZ, 0) < 0) {
-                perror("msgsnd");
-                exit(-4);
-            }
+
+        } else if (strcmp(command, "quit") == 0) {
+            my_msg.info.comm_type = DISCONNECT;
+            send_message(&my_msg);
             exit(0);
         }
     }
